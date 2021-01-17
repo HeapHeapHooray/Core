3822a4578e6cec760076aa0a4795c2240065dfe3
HeapHeapHooray <59413175+HeapHeapHooray@users.noreply.github.com>
2021-01-17 13:14:03 -0300

    Add files via upload

--- Core/code/CoreDatabase/DefaultDatabases.py
+++ Core/code/CoreDatabase/DefaultDatabases.py
@@ -3,6 +3,5 @@ import CoreFileSystem
 from .DatabasesDirectory import DatabasesDirectory
 from . import DatabasesDirectoryFactory
 
-class DefaultDatabases:
-    def get_default_databases() -> DatabasesDirectory:
-        return DatabasesDirectoryFactory.get_databases_directory_from(CoreFileSystem.CoreDirectories.get_default_databases_directory_path())
+def get_default_databases() -> DatabasesDirectory:
+    return DatabasesDirectoryFactory.get_databases_directory_from(CoreFileSystem.CoreDirectories.get_default_databases_directory_path())
